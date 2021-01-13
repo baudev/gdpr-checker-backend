2f94f71e659e067cd084474f0135e7299e31a45f
Baudev <baudev.contact@gmail.com>
2021-01-13 01:23:53 +0100

    Adds config service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ReportController } from './report/report.controller';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [],
+  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  controllers: [ReportController],
   providers: [],
 })
 export class AppModule {}
