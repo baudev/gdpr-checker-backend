7b03850405ea03ea24dda6d414e41eb60d5d7dbf
Baudev <baudev.contact@gmail.com>
2021-01-15 21:57:10 +0100

    Fixes bug concerning env variables

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -11,11 +11,11 @@ const data: any = dotenv.parse(fs.readFileSync(`.env`));
 
 export const config: TypeOrmModuleOptions = {
   type: 'postgres',
-  host: data.DATABASE_HOST,
-  port: data.DATABASE_PORT,
-  username: data.DATABASE_USERNAME,
-  password: data.DATABASE_PASSWORD,
-  database: data.DATABASE_NAME,
+  host: process.env.DATABASE_HOST || data.DATABASE_HOST,
+  port: process.env.DATABASE_PORT || data.DATABASE_PORT,
+  username: process.env.DATABASE_USERNAME || data.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD || data.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME || data.DATABASE_NAME,
   migrationsTableName: 'migration_table',
   migrations: [join(__dirname, '**', '*.ts')],
   cli: {
