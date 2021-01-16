8bc35b8dcbf7b449d1eeac6613aaf6844eb1891e
Baudev <baudev.contact@gmail.com>
2021-01-16 11:54:34 +0100

    Lowercase the domain parameter in UrlPipe

--- src/pipe/url.pipe.ts
+++ src/pipe/url.pipe.ts
@@ -12,7 +12,7 @@ import {
 export class UrlPipe implements PipeTransform<string, string> {
   transform(value: string, metadata: ArgumentMetadata): string {
     const regex = /(http(s)?:\/\/)?([a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5})(\/.*)?$/m;
-    const matches = regex.exec(value);
+    const matches = regex.exec(value.toLowerCase());
     // check if valid domain
     if (matches == null) {
       throw new BadRequestException('Invalid link');
